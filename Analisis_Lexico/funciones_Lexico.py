be29eb8b0d88c283c51f526f309f509bd6bfddab
AlejandroMoral3s <alejandrousac60@gmail.com>
2023-05-25 10:08:15 -0600

    comentando funciones de separadores

--- Analisis_Lexico/funciones_Lexico.py
+++ Analisis_Lexico/funciones_Lexico.py
@@ -3,11 +3,11 @@ from separadores_Lexico import *  #Importando la clase en donde se encuentran lo
 
 string = 'hola,      como,       estas'
 
-def separar_por_coma(linea):
+def separar_por_coma(linea):  #Funcion que proporciona una separacion por comas
     
-    contenedor = re.split('(,)', linea)
-    nuevo_string = ' '.join(contenedor)
-    return nuevo_string
+    contenedor = re.split('(,)', linea) #Separar por comas y colocando la separacion en una lista, pero conservando el caracter.
+    nuevo_string = ' '.join(contenedor) #Uniendo la lista en un STRING separado por espacios
+    return nuevo_string #devolviendo el string
 
 def separar_por_espacio(linea):
 
