4df546cc17cd333b01d910f3ab28e02633183da2
AlejandroMoral3s <alejandrousac60@gmail.com>
2023-07-12 23:24:07 -0600

    quitando prueba

--- Analizador.py
+++ Analizador.py
@@ -43,7 +43,7 @@ with open('ReceptorLineas.txt', 'r') as f:
 
         if (existeRegla==True):
             print("Proceso SINTACTICO finalizado sin ningun error!\n")
-            print("PRUEBA DIFERENTE DE BRANCHES")
+            
         else:
             print(f"Se encontraron errores sintacticos en la linea [ {contador} ], por favor verificar.\n")     
 
